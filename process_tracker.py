bbeeb6f79793765a1a2181758caca92a02518550
dscrn <mbhendricks.91@gmail.com>
2022-08-11 22:47:48 -0500

    More development

--- process_tracker.py
+++ process_tracker.py
@@ -1,14 +1,8 @@
 import psutil
 import pandas as pd
+import time
 
-
-if __name__ == '__main__':
-
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.expand_frame_repr', False)
-    pd.set_option('max_colwidth', None)
-    pd.set_option('display.max_rows', None)
-
+def get_processes():
     process_list = []
     for proc in psutil.process_iter():
         process_list.append(proc.as_dict(attrs=['pid', 'name', 'cpu_percent', 'create_time', 'memory_percent', 'memory_info']))
@@ -19,6 +13,24 @@ if __name__ == '__main__':
         processes = pd.concat([processes, pd.DataFrame.from_dict(each)], ignore_index=True)
 
     processes = processes.groupby('name').sum().sort_values(by=['cpu_percent', 'memory_percent'], ascending=False)
-    # processes = processes[processes['cpu_percent'] == 0]
-    print(processes)
+    return processes
+
+
+def driver():
+    while True:
+        procs = get_processes()
+        time.sleep(30)
+        return procs
+
+
+if __name__ == '__main__':
+
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.expand_frame_repr', False)
+    pd.set_option('max_colwidth', None)
+    pd.set_option('display.max_rows', None)
+
+    stats = driver()
+    print(stats)
+
 
